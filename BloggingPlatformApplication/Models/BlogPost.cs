b50eff36346568f12d043f933efe80f0ae1eacb0
Yumacorn <reachryanmui@gmail.com>
2023-10-01 15:15:44 -0400

    20231001 Initial model for User and BlogPost

---
+++ BloggingPlatformApplication/Models/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿namespace BloggingPlatformApplication.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime timestamp_creation { get; set; }
+        public DateTime timestamp_updated { get; set;}
+
+    }
+}
