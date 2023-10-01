cc43a0f2560a809e1422da63c318b30478011229
Yumacorn <reachryanmui@gmail.com>
2023-10-01 15:16:47 -0400

    20231001 Add basic required field validation

--- BloggingPlatformApplication/Models/BlogPost.cs
+++ BloggingPlatformApplication/Models/BlogPost.cs
@@ -1,12 +1,16 @@
-﻿namespace BloggingPlatformApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloggingPlatformApplication.Models
 {
     public class BlogPost
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Content { get; set; }
-        public DateTime timestamp_creation { get; set; }
-        public DateTime timestamp_updated { get; set;}
+        public DateTime timestamp_creation { get; }
+        public DateTime timestamp_updated { get; }
 
     }
 }
